4a643e6a4c0b6683f967e346e2ebc66602d6cc4b
RoWiz09 <139277027+RoWiz09@users.noreply.github.com>
2024-01-19 17:35:16 -0600

    Update to tick-tack-toe

--- tick_tack_toe.java
+++ tick_tack_toe.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.awt.Desktop;
+import java.awt.desktop.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class tick_tack_toe {
     public static String[] StringBoard = new String[9];
+    public static int[] board = new int[9];
     public static void main(String[] args) {
-        int[] board = new int[9];
         for (int item = 0; item<9; item++){
             board[item] = 0;
             StringBoard[item] = "_";
@@ -15,7 +21,7 @@ public class tick_tack_toe {
     public static void gameLoop(){
         String visualBoard = "a "+ StringBoard[0] +"|"+ StringBoard[1] +"|"+ StringBoard[2] +" \nb "+ StringBoard[3] +"|"+ StringBoard[4] +"|"+ StringBoard[5] +"\nc "+ StringBoard[6] +"|"+ StringBoard[7] +"|"+ StringBoard[8] +" \n  1 2 3";
         String xo = "x";
-        for(int i=0; i<9; i++){
+        while (true){
             int pos = getInput(visualBoard, xo);
             StringBoard[pos]=xo;
             visualBoard = "a "+ StringBoard[0] +"|"+ StringBoard[1] +"|"+ StringBoard[2] +" \nb "+ StringBoard[3] +"|"+ StringBoard[4] +"|"+ StringBoard[5] +"\nc "+ StringBoard[6] +"|"+ StringBoard[7] +"|"+ StringBoard[8] +" \n  1 2 3";
@@ -23,40 +29,42 @@ public class tick_tack_toe {
                 xo = "o";
             else
                 xo = "x";
+            if ((board[0] == 1 & board[1] == 1 & board[2] == 1) || (board[3] == 1 & board[4] == 1 & board[5] == 1) || (board[6] == 1 & board[7] == 1 & board[8] == 1) || (board[0] == 1 & board[3] == 1 & board[6] == 1) || (board[1] == 1 & board[4] == 1 & board[7] == 1) || (board[2] == 1 & board[5] == 1 & board[8] == 1) || (board[0] == 1 & board[4] == 1 & board[8] == 1) || (board[2] == 1 & board[4] == 1 & board[6] == 1)){print(visualBoard);winning("x");}
+            else if ((board[0] == 2 & board[1] == 2 & board[2] == 2) || (board[3] == 2 & board[4] == 2 & board[5] == 2) || (board[6] == 2 & board[7] == 2 & board[8] == 2) || (board[0] == 2 & board[3] == 2 & board[6] == 2) || (board[1] == 2 & board[4] == 2 & board[7] == 2) || (board[2] == 2 & board[5] == 2 & board[8] == 2) || (board[0] == 2 & board[4] == 2 & board[8] == 2) || (board[2] == 2 & board[4] == 2 & board[6] == 2)){print(visualBoard);winning("o");}
+            for(int space = 0; space<9; space++){
+                if(board[space] == 0){
+                    break;
+                }
+                else if(space == 8){draw();}
+            }
         }
     }
     public static int getInput(String visualBoard, String xo){
         System.out.println(visualBoard + "\n where do you want to place your " + xo);
         Scanner tinput = new Scanner(System.in);
         String input = tinput.nextLine(); 
-        System.out.println(input);
         int pos = 9;
-        if(input.equals("a1"))
-            pos = 0;
-        if(input.equals("a2"))
-            pos = 1;
-        if(input.equals("a3"))
-            pos = 2;
-        if(input.equals("b1"))
-            pos = 3;
-        if(input.equals("b2"))
-            pos = 4;
-        if(input.equals("b3"))
-            pos = 5;
-        if(input.equals("c1"))
-            pos = 6;
-        if(input.equals("c2"))
-            pos = 7;
-        if(input.equals("c3"))
-            pos = 8;
-        if(pos == 9)
-        {
-            System.out.println("please put in an ACTUAL POSITION ON THE BOARD LILLY!" + pos);
-            System.exit(1);
+        if(input.equals("a1")){pos = 0;}
+        else if(input.equals("a2")){pos = 1;}
+        else if(input.equals("a3")){pos = 2;}
+        else if(input.equals("b1")){pos = 3;}
+        else if(input.equals("b2")){pos = 4;}
+        else if(input.equals("b3")){pos = 5;}
+        else if(input.equals("c1")){pos = 6;}
+        else if(input.equals("c2")){pos = 7;}
+        else if(input.equals("c3")){pos = 8;}
+        if(pos == 9){System.out.println("please put in an ACTUAL POSITION on the board, LILLY!");System.exit(1);}
+        else if(board[pos] != 0){
+        System.out.println("Please play a valid move!");
+        pos = getInput(visualBoard, xo);
         }
+        int player = 0;
+        if(xo.equals("x")){player = 1;}
+        else{player = 2;}
+        board[pos] = player;
         return pos;
     }
-    public static print(String message){
-        System.out.println(message);
-    }
+    public static void winning(String team){print(team + " wins!");String url = "https://docs.google.com/document/d/11Nqxg_lOjFnlwF-7qWdbGGRtWyfXfzX5gnkFMG3S_RE/edit?usp=sharing"; Desktop desktop = Desktop.getDesktop(); try{URI uri = new URI(url); desktop.browse(uri);System.exit(1);} catch (IOException excp) {excp.printStackTrace();} catch (URISyntaxException excp) {excp.printStackTrace();} try{TimeUnit.SECONDS.sleep(5);}catch (InterruptedException e) {Thread.currentThread().interrupt();} while (true){String url2 = "https://www.youtube.com/watch?v=AuoH0vz3Mqk"; try{URI uri = new URI(url); desktop.browse(uri);System.exit(1);} catch (IOException excp) {excp.printStackTrace();} catch (URISyntaxException excp) {excp.printStackTrace();} try{TimeUnit.MILLISECONDS.sleep(20);}catch (InterruptedException e) {Thread.currentThread().interrupt();}}}
+    public static void draw(){print("Game over! \nYou failed to confirm the kill..."); gameLoop();}
+    public static void print(String message){System.out.println(message);}
 }
